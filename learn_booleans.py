dbebd36ec0fcff5ba262e98fc8d02a5c81e066eb
epadilla35 <142930401+epadilla35@users.noreply.github.com>
2023-09-20 17:53:40 +0000

    Initial commit

---
+++ learn_booleans.py
@@ -0,0 +1,12 @@
+def learn_booleans():
+  print("I like booleans")
+    ###########################Booleans#######################################
+  
+  # Booleans Practice #1
+  # Make a comparison that returns a boolean and store the result (True/False) in a variable called test
+  
+  # Booleans Practice #2
+  # Check if 17834/34 is greater than 87*56 and print the boolean result to the screen using print()
+  
+  # Booleans Practice #3
+  # Check if the square root of 25 is equal to 5 and display the result (boolean) on the screen using print()
---
+++ main.py
@@ -0,0 +1,13 @@
+
+from learn_dictionaries import *
+from learn_tuples import *
+from learn_sets import *
+from learn_booleans import *
+
+learn_dictionaries()
+learn_tuples()
+learn_sets()
+learn_booleans()
+
+
+###############################Proceed to last slide#################################
